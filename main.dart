97a37702ecb3ef43f19a0367d3a3e2f7d6ea77c0
mohammad6202 <m.almsedin@gmail.com>
2022-08-25 18:10:55 +0300

    this is my first upload

--- main.dart
+++ main.dart
@@ -1,4 +1,15 @@
 void main() {
-  String myName = "Mohammad Ibrahim";
-  print(myName);
+  Map myInformation = {
+    "first_name": "Mohammad",
+    "Last_name": "Almsedin",
+    "age": 22,
+    "education_info": {
+      "University": "JUST",
+      "Faculity": "IT",
+      "Department": "Network and Security Engineer",
+    },
+    "family memebers": 7,
+    "linkedIn account":
+        "https://www.linkedin.com/in/mohammad-ibrahim-17295a208",
+  };
 }
